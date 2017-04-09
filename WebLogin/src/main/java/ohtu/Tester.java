3c77c17f8d24c8293dd3ce313378c72a43dd7d50
NorthernLion <joppetaja@gmail.com>
2017-04-09 16:02:50 +0300

    a

--- WebLogin/src/main/java/ohtu/Tester.java
+++ WebLogin/src/main/java/ohtu/Tester.java
@@ -3,21 +3,21 @@ package ohtu;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Tester {
 
     public static void main(String[] args) {
-        WebDriver driver = new HtmlUnitDriver();
+        WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
         
-        //sleep(2);
+        sleep(2);
         
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
-        //sleep(2);
+        sleep(2);
 
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
@@ -25,73 +25,85 @@ public class Tester {
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
         
-        //sleep(2);
+        sleep(2);
         element.submit();
+
+        sleep(3);
 
         driver.get("http://localhost:4567");
 
-        //sleep(2);
+        sleep(2);
 
         element = driver.findElement(By.linkText("login"));
         element.click();
 
-        //sleep(2);
+        sleep(2);
 
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("eri");
+        element.sendKeys("vaara");
         element = driver.findElement(By.name("login"));
 
-        //sleep(2);
+        sleep(2);
         element.submit();
+
+        sleep(3);
 
         driver.get("http://localhost:4567");
 
-        //sleep(2);
+        sleep(2);
 
-         element = driver.findElement(By.linkText("login"));
+        element = driver.findElement(By.linkText("login"));
         element.click();
 
-        //sleep(2);
+        sleep(2);
 
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekkaaaaaa");
+        element.sendKeys("eiole");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
+        element.sendKeys("eitatakaan");
         element = driver.findElement(By.name("login"));
 
-        //sleep(2);
+        sleep(2);
         element.submit();
 
+        sleep(3);
 
         driver.get("http://localhost:4567");
 
-        //sleep(2);
-
+        sleep(2);
 
         element = driver.findElement(By.linkText("register new user"));
         element.click();
 
-        //sleep(2);
+        sleep(2);
 
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka2");
+        element.sendKeys("nakki");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep2");
+        element.sendKeys("nakki");
         element = driver.findElement(By.name("passwordConfirmation"));
-        element.sendKeys("akkep2");
+        element.sendKeys("nakki");
         element = driver.findElement(By.name("signup"));
-        //sleep(2);
+
+        sleep(2);
         element.submit();
+
+        sleep(2);
 
         element = driver.findElement(By.linkText("continue to application mainpage"));
         element.click();
 
+        sleep(2);
+
         element = driver.findElement(By.linkText("logout"));
         element.click();
-        //sleep(3);
-        
+
+        sleep(3);
+
+
+
         driver.quit();
     }
     
--- WebLogin/src/test/java/ohtu/Stepdefs.java
+++ WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -1,19 +1,17 @@
 package ohtu;
 
 import cucumber.api.java.After;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.By;
+import static org.junit.Assert.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
-import static org.junit.Assert.assertTrue;
 
 public class Stepdefs {
-    WebDriver driver = new HtmlUnitDriver();
+    WebDriver driver = new ChromeDriver();
     String baseUrl = "http://localhost:4567";
     
     @Given("^login is selected$")
@@ -21,6 +19,21 @@ public class Stepdefs {
         driver.get(baseUrl);
         WebElement element = driver.findElement(By.linkText("login"));       
         element.click();          
+    } 
+
+    @When("^username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
+    public void username_and_password_are_given(String username, String password) throws Throwable {
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("login"));
+        element.submit();  
+    }
+
+    @Then("^system will respond \"([^\"]*)\"$")
+    public void system_will_respond(String pageContent) throws Throwable {
+        assertTrue(driver.getPageSource().contains(pageContent));
     }
     
     @When("^correct username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
@@ -63,104 +76,5 @@ public class Stepdefs {
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
         element.submit();  
-    }
-
-    @When("^nonexistent username \"([^\"]*)\" is given$")
-    public void nonexistentUsernameIsGiven(String username) throws Throwable {
-        logInWith(username, "testpassword");
-    }
-
-    @Given("^new user is selected$")
-    public void newUserIsSelected() throws Throwable {
-        driver.get(baseUrl);
-        WebElement element = driver.findElement(By.linkText("register new user"));
-        element.click();
-    }
-
-    @Then("^user \"([^\"]*)\" is not created and error \"([^\"]*)\" is reported$")
-    public void userIsNotCreatedAndErrorIsReported(String username, String message) throws Throwable {
-        pageHasContent(message);
-        assertTrue(driver.getPageSource().contains("Create username and give password"));
-        WebElement element = driver.findElement(By.name("username"));
-        element.sendKeys(username);
-        element = driver.findElement(By.name("password"));
-        element.sendKeys("testpassword!");
-        element = driver.findElement(By.name("passwordConfirmation"));
-        element.sendKeys("testpassword!");
-        element = driver.findElement(By.name("signup"));
-        element.submit();
-        pageHasContent("is already taken");
-    }
-
-    @When("^valid username \"([^\"]*)\" and valid password \"([^\"]*)\" are given$")
-    public void validUsernameAndValidPasswordAreGiven(String username, String password) throws Throwable {
-        assertTrue(driver.getPageSource().contains("Create username and give password"));
-        WebElement element = driver.findElement(By.name("username"));
-        element.sendKeys(username);
-        element = driver.findElement(By.name("password"));
-        element.sendKeys(password);
-        element = driver.findElement(By.name("passwordConfirmation"));
-        element.sendKeys(password);
-        element = driver.findElement(By.name("signup"));
-        element.submit();
-    }
-
-    @Then("^user is created and message \"([^\"]*)\" is shown$")
-    public void userIsCreatedAndMessageIsShown(String message) throws Throwable {
-        this.pageHasContent(message);
-    }
-
-    @When("^invalid username \"([^\"]*)\" and valid password \"([^\"]*)\" are given$")
-    public void invalidUsernameAndValidPasswordAreGiven(String username, String password) throws Throwable {
-        assertTrue(driver.getPageSource().contains("Create username and give password"));
-        WebElement element = driver.findElement(By.name("username"));
-        element.sendKeys(username);
-        element = driver.findElement(By.name("password"));
-        element.sendKeys(password);
-        element = driver.findElement(By.name("passwordConfirmation"));
-        element.sendKeys(password);
-        element = driver.findElement(By.name("signup"));
-        element.submit();
-    }
-
-    @When("^valid  username \"([^\"]*)\" and invalid password \"([^\"]*)\" are given$")
-    public void validUsernameAndInvalidPasswordAreGiven(String username, String password) throws Throwable {
-        assertTrue(driver.getPageSource().contains("Create username and give password"));
-        WebElement element = driver.findElement(By.name("username"));
-        element.sendKeys(username);
-        element = driver.findElement(By.name("password"));
-        element.sendKeys(password);
-        element = driver.findElement(By.name("passwordConfirmation"));
-        element.sendKeys(password);
-        element = driver.findElement(By.name("signup"));
-        element.submit();
-    }
-
-    @Given("^user \"([^\"]*)\" is created$")
-    public void userIsCreated(String name) throws Throwable {
-        this.newUserIsSelected();
-        assertTrue(driver.getPageSource().contains("Create username and give password"));
-        WebElement element = driver.findElement(By.name("username"));
-        element.sendKeys(name);
-        element = driver.findElement(By.name("password"));
-        element.sendKeys("tester12");
-        element = driver.findElement(By.name("passwordConfirmation"));
-        element.sendKeys("tester12");
-        element = driver.findElement(By.name("signup"));
-        element.submit();
-    }
-
-    @And("^invalid password confirmation \"([^\"]*)\" is given$")
-    public void invalidPasswordConfirmationIsGiven(String confirm) throws Throwable {
-        this.newUserIsSelected();
-        assertTrue(driver.getPageSource().contains("Create username and give password"));
-        WebElement element = driver.findElement(By.name("username"));
-        element.sendKeys(confirm);
-        element = driver.findElement(By.name("password"));
-        element.sendKeys("tester12");
-        element = driver.findElement(By.name("passwordConfirmation"));
-        element.sendKeys(confirm);
-        element = driver.findElement(By.name("signup"));
-        element.submit();
-    }
+    } 
 }
